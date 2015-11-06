2a68df0e8d405c836051af540ea7f09f596ed9f9
Austen <rawleag@gmail.com>
2015-11-06 13:51:12 -0800

    app.js

--- public/js/angular/icecreams/app.js
+++ public/js/angular/icecreams/app.js
@@ -1,20 +1,24 @@
-angular
+(function(){
+
+  angular
   .module('myapp', [
     'ngRoute'
   ])
 
-.config(['$routeProvider', function($routeProvider){
-  $routeProvider
-    .when('/', {
-      templateUrl: '../views/icecreams/index.html',
-      controller: 'icecreamController'
-    })
-    .when('/:id', {
-      templateUrl:'../views/icecreams/icecream.html',
-      controller: 'individualIceCreamController'
-    })
-}])
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider
+      .when('/', {
+        templateUrl: '../views/icecreams/index.html',
+        controller: 'icecreamController'
+      })
+      .when('/:id', {
+        templateUrl:'../views/icecreams/icecream.html',
+        controller: 'individualIceCreamController'
+      })
+  }])
 
-.config(function($locationProvider) {
-  $locationProvider.html5Mode(true);
-});
+  .config(function($locationProvider) {
+    $locationProvider.html5Mode(true);
+  });
+
+})();
